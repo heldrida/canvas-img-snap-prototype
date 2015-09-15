4ab15f3a1a1c3368575c4a73119787d829faf2bc
Helder <helder@lamoulade.com>
2015-09-15 17:28:01 +0100

    enabled touch

--- js/app.js
+++ js/app.js
@@ -17,12 +17,16 @@
 		setVars: function () {
 
 			this.stage = new createjs.Stage('myCanvas');
+			this.stage.canvas.width = 320;
+			this.stage.canvas.height = 240;
+			createjs.Touch.enable(this.stage);
+
 			this.el_my_result = document.getElementById('my_result');
 			this.el_take_snapshot = document.querySelector('.take_snapshop');
 			this.container = new createjs.Container();
 
-			this.stage.canvas.width = 320;
-			this.stage.canvas.height = 240;
+			this.dragBox = new createjs.Shape(new createjs.Graphics().beginFill("#ff0000").drawRect(0, 0, this.stage.canvas.width, this.stage.canvas.height))
+			this.stage.addChild(this.dragBox);
 
 		},
 
@@ -39,7 +43,7 @@
 				}.bind(this));
 
 			}.bind(this));
-
+			/*
 			this.container.on('mousedown', function (evt) {
 
 				var offset = {
@@ -53,6 +57,25 @@
 				});
 
 			});
+			*/
+
+			this.dragBox.addEventListener("mousedown", function (event) {
+
+				var offset = new createjs.Point();
+
+				offset.x = this.stage.mouseX - this.container.x;
+				offset.y = this.stage.mouseY - this.container.y;
+
+				event.target.addEventListener("pressmove", function (event) {
+
+					this.container.x = event.stageX - offset.x;
+					this.container.y = event.stageY - offset.y;
+
+					console.log('x: ' + this.container.x + ', y: ' + this.container.y);
+
+				}.bind(this));
+
+			}.bind(this));
 
 		},
 
@@ -74,18 +97,16 @@
 
 		take_snapshot_to_canvas: function (data_uri) {
 
-			/*
 			// reset
-			container.removeAllChildren();
-			container.removeAllEventListeners();
-			stage.removeAllChildren();
- 			stage.removeAllEventListeners();
-			 */
+			this.container.removeAllChildren();
+			//container.removeAllEventListeners();
+			//this.stage.removeAllChildren();
+ 			//stage.removeAllEventListeners();
 
 			var image = new createjs.Bitmap(data_uri),
 				shape = new createjs.Shape();
 
-			shape.graphics.beginFill("#ff0000").drawRect(0, 0, 10, 10);
+			shape.graphics.beginFill("#FFCC00").drawRect(0, 0, 10, 10);
 
 			this.container.addChild(image, shape);
 
