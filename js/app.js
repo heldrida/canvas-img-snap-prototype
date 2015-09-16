06ba38578829395eed74028dac65f2dcc660aa08
Helder <helder@lamoulade.com>
2015-09-16 10:55:19 +0100

    created handler shapes

--- js/app.js
+++ js/app.js
@@ -24,11 +24,14 @@
 			this.el_my_result = document.getElementById('my_result');
 			this.el_take_snapshot = document.querySelector('.take_snapshot');
 			this.container = new createjs.Container();
+			this.handler_container = new createjs.Container();
 
 			this.dragBox = new createjs.Shape(new createjs.Graphics().beginFill("#FFFFFF").drawRect(0, 0, this.stage.canvas.width, this.stage.canvas.height))
 			this.stage.addChild(this.dragBox);
 
 			this.btnPrint = document.querySelector('.btn-print');
+
+			this.snapshot = null;
 
 		},
 
@@ -59,6 +62,9 @@
 
 					this.container.x = event.stageX - offset.x;
 					this.container.y = event.stageY - offset.y;
+
+					this.handler_container.x = this.container.x;
+					this.handler_container.y = this.container.y;
 
 				}.bind(this));
 
@@ -91,9 +97,9 @@
 			// reset
 			this.container.removeAllChildren();
 
-			var image = new createjs.Bitmap(data_uri);
+			this.snapshot = new createjs.Bitmap(data_uri);
 
-			this.container.addChild(image);
+			this.container.addChild(this.snapshot);
 
 			this.stage.addChild(this.container);
 
@@ -109,15 +115,20 @@
 
 		placeResizeHandlers: function () {
 
-			var shape = new createjs.Shape();
+			var shape_size = { w: 10, h: 10 },
+				shp1 = new createjs.Shape(),
+				shp2 = new createjs.Shape(),
+				shp3 = new createjs.Shape(),
+				shp4 = new createjs.Shape();
 
-			shape.graphics.beginFill("#FFCC00").drawRect(0, 0, 10, 10);
+			shp1.graphics.beginFill("#FFCC00").drawRect(0, 0, shape_size.w, shape_size.h);
+			shp2.graphics.beginFill("#00FFCC").drawRect(0, this.snapshot.image.height - shape_size.h, shape_size.w, shape_size.h);
+			shp3.graphics.beginFill("#CC00FF").drawRect(this.snapshot.image.width - shape_size.w, 0, shape_size.w, shape_size.h);
+			shp4.graphics.beginFill("#FF00CC").drawRect(this.snapshot.image.width - shape_size.w, this.snapshot.image.height - shape_size.h, shape_size.w, shape_size.h);
 
-			this.stage.setChildIndex(shape, this.stage.getNumChildren() + 1);
+			this.handler_container.addChild(shp1, shp2, shp3, shp4);
 
-			this.container.addChild(shape);
-
-			console.log(this.container);
+			this.stage.addChild(this.handler_container);
 
 		},
 
