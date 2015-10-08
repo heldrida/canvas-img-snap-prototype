640f290fd1c5265cedeea9227a6147dd327760de
Helder <helder@lamoulade.com>
2015-10-08 18:03:07 +0100

    created the mobile version (wip)

--- js/app.js
+++ js/app.js
@@ -103,6 +103,23 @@
 
 			this.form = document.querySelector('form[name="email"]');
 
+			if (window.innerWidth <= 767) {
+
+				var mySwiper = new Swiper ('.swiper-container', {
+					// Optional parameters
+					direction: 'horizontal',
+					loop: true,
+
+					// If we need pagination
+					pagination: '.swiper-pagination',
+
+					// Navigation arrows
+					nextButton: '.swiper-button-next',
+					prevButton: '.swiper-button-prev'
+				});
+
+			}
+
 		},
 
 		setListeners: function () {
